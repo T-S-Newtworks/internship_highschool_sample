e464a5f60fac4230900ad64b8bcdcc76d3472917
田鍋 佑太 <yuta@tsn.co.jp>
2022-08-19 18:16:28 +0900

    update

--- Script1.js
+++ Script1.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+$(document).ready(function(){
+    photoSize();
+});
+
+function photoSize(){
+    if(window.matchMedia('(min-width: 768px)').matches) {
+        $(".photo").colorbox({
+            width:"60%",
+        });
+    }else{
+        $(".photo").colorbox({
+            width:"100%",
+        });
+}
+}
